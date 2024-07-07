46b36f22c45b575b92ec01a206161ac79b97eae2
YuuMacMini <a815261s@gmail.com>
2024-07-07 20:55:37 +0800

    Initial Commit

---
+++ APP_Competition_2024/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  APP_Competition_2024
+//
+//  Created by STUDIO A on 2024/7/7.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
